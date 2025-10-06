5337b20f6a908d8133709c36ef0feb34f7c8e4b3
HumourSpeech <mishranitin6076@gmail.com>
2025-10-06 23:13:41 +0530

    Added field extraction with labels

--- assisstants/extractor/fields_extractor.py
+++ assisstants/extractor/fields_extractor.py
@@ -3,19 +3,25 @@ from assisstants.exception.exception import AssisstantException
 from assisstants.logging.logger import logging
 
 class ExtractFields:
-    def extract(self, text):
+    def extract(self,label, text):
         try:
             logging.info("Field Extraction Started")
             import re
 
-            # Phone (Indian-style 10 digits, optional +91 or 0)
-            phone_no = re.findall(r'(?:(?:\+91|0)?[\s\-]?)?[6-9]\d{9}', text)
+            if label == "Phone Number":
+                # Phone (Indian-style 10 digits, optional +91 or 0)
+                phone_no = re.findall(r'(?:(?:\+91|0)?[\s\-]?)?[6-9]\d{9}', text)
+                return phone_no[0] if phone_no else []
 
-            # Account numbers (usually 11 to 18 digits)
-            account_no = re.findall(r'\b\d{11,18}\b', text)
+            if label == "Account Number":
+                # Account numbers (usually 11 to 18 digits)
+                account_no = re.findall(r'\b\d{11,18}\b', text)
+                return account_no[0] if account_no else []
 
-            # Amounts (₹, Rs., or plain numbers with commas/decimals)
-            amount = re.findall(r'(?:₹|Rs\.?|INR)?[\s]?[0-9,]+(?:\.\d{1,2})?', text)
+            if label == "Amount":
+                # Amounts (₹, Rs., or plain numbers with commas/decimals)
+                amount = re.findall(r'(?:₹|Rs\.?|INR)?[\s]?[0-9,]+(?:\.\d{1,2})?', text)
+                return amount[0] if amount else []
 
             # Names
             # Need to fine tune this NER model with specific Bank database for more better accuracy
@@ -28,11 +34,6 @@ class ExtractFields:
 
             logging.info("Field Extraction Completed")
 
-            return {
-                name[0] if name else None,
-                phone_no[0] if phone_no else None,
-                amount [0] if amount else None,
-                account_no[0] if account_no else None,
-            }
+            return name[0] if name else []
         except Exception as e:
             raise AssisstantException(e, sys)
